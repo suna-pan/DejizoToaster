a3b6297a60837a20b6be365626e0810728468a54
suna_pan <lisknkknk.al.g2k13@gmail.com>
2015-10-09 20:14:30 +0900

    Get dict ID

--- DejizoToaster/Form1.cs
+++ DejizoToaster/Form1.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace DejizoToaster
 {
@@ -16,6 +20,23 @@ namespace DejizoToaster
         public Form1()
         {
             InitializeComponent();
+
+            var id = LoopUp("toast");
+            Debug.WriteLine(id);
+        }
+
+
+        private int LoopUp(string word)
+        {
+            var xml = DejizoSearch(word);
+            var ns = xml.Name.Namespace;
+
+            var itemCount = xml.Descendants(ns + "ItemCount").First().Value;
+            if (Int32.Parse(itemCount) < 1)
+                return -1;
+
+            var itemId = xml.Descendants(ns + "ItemID").First().Value;
+            return Int32.Parse(itemId);
         }
 
 
@@ -27,6 +48,21 @@ namespace DejizoToaster
             var res = req.GetResponse();
 
             return res;
+        }
+
+
+        private XElement DejizoSearch(string word)
+        {
+            var encWord = HttpUtility.UrlEncode(word);
+            var queryUri = "http://public.dejizo.jp/NetDicV09.asmx/SearchDicItemLite?Dic=EJdict&Word="
+                           + encWord + "&Scope=HEADWORD&Match=STARTWITH&Merge=AND&Prof=XHTML&PageSize=1&PageIndex=0";
+            Debug.WriteLine(queryUri);
+
+            var res = HttpGet(queryUri);
+            var resReader = XmlReader.Create(res.GetResponseStream());
+            var docs = XElement.Load(resReader);
+
+            return docs;
         }
     }
 }
