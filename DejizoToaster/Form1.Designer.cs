991cf04227e29bb11db51f67cc83a89104bdaf62
suna_pan <lisknkknk.al.g2k13@gmail.com>
2015-10-09 22:58:33 +0900

    Add icon

--- DejizoToaster/Form1.Designer.cs
+++ DejizoToaster/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(120, 25);
+            this.ClientSize = new System.Drawing.Size(120, 0);
             this.Name = "Form1";
             this.ShowInTaskbar = false;
             this.Text = "Form1";
